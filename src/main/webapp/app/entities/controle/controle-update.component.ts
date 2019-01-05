490504242f964d6d83a9bd0948e7f6e1390b0793
jaironsousa1 <jaironsousa@gmail.com>
2019-01-05 08:15:31 -0300

    Ajuste Ajuste no formulario de controle e na listagem

--- src/main/webapp/app/entities/controle/controle-update.component.ts
+++ src/main/webapp/app/entities/controle/controle-update.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import * as moment from 'moment';
+import 'moment/locale/pt-br';
 
 import { IControle } from 'app/shared/model/controle.model';
 import { ControleService } from './controle.service';
@@ -16,13 +17,20 @@ export class ControleUpdateComponent implements OnInit {
     isSaving: boolean;
     dataDp: any;
 
+    horas = ['0:00', '1:00', '', ''];
+
     constructor(protected controleService: ControleService, protected activatedRoute: ActivatedRoute) {}
 
     ngOnInit() {
+        moment.locale('pt-BR');
         this.isSaving = false;
         this.activatedRoute.data.subscribe(({ controle }) => {
             this.controle = controle;
         });
+        if (!this.controle.id) {
+            // this.controle.data = moment(moment(), "DD-MM-YYYY");
+            this.controle.corPulseira = '#FFF';
+        }
     }
 
     previousState() {
@@ -31,6 +39,7 @@ export class ControleUpdateComponent implements OnInit {
 
     save() {
         this.isSaving = true;
+        console.log(this.controle);
         if (this.controle.id !== undefined) {
             this.subscribeToSaveResponse(this.controleService.update(this.controle));
         } else {
